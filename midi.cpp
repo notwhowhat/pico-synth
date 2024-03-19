761c156135e44dd23a11cd07ddc2b35640c18d81
whowhat <jakobthefoodmonster@gmail.com>
2024-03-19 16:54:05 +0100

    working midi in kindof

--- midi.cpp
+++ midi.cpp
@@ -1,33 +1,26 @@
 #include <Arduino.h>
 #include "midi.h"
 
-#define CHANNEL 0
+//#define CHANNEL 0
 
-struct _midi_in {
-    byte cmd[3];
-    int counter;
-
-    // for regestering inputs
-    byte keys[128];
-    byte prev_keys[128];
-
-};
-
-typedef struct _midi_in midi_in;
+//typedef struct _midi_in midi_in;
 
 midi_in *midi_in_init() {
     midi_in *p= (midi_in*)malloc(sizeof(midi_in));
     if (p != NULL) {
+
         p->cmd[0] = 0;
         p->cmd[1] = 0;
         p->cmd[2] = 0;
 
         p->counter = 0;
 
-        for (int i; i < 127; i++) {
+        for (int i = 0; i < 127; i++) {
             p->keys[i] = 0;
             p->prev_keys[i] = 0;
         }
+    } else {
+        Serial.println("WRONG");
     }
     return p;
     
@@ -49,7 +42,9 @@ midi_in *midi_in_init() {
     //return &midi;
 }
 
-byte midi_in_get_raw() {
+static byte midi_in_get_raw() {
+    while (Serial1.available() < 1) {
+    }
     int buffer = Serial1.read();
     if (buffer < 0) {
         return 0;
@@ -58,36 +53,40 @@ byte midi_in_get_raw() {
 }
 
 void midi_in_note_on(midi_in *pmidi, byte pitch, byte velocity) {
-    midi_in midi = *pmidi;
-    midi.prev_keys[pitch] = midi.keys[pitch];
-    midi.keys[pitch] = velocity;
+#ifdef PRINT
+    Serial.println("on");
+#endif
+    pmidi->prev_keys[pitch] = pmidi->keys[pitch];
+    pmidi->keys[pitch] = velocity;
 }
 
 void midi_in_note_off(midi_in *pmidi, byte pitch, byte velocity) {
-    midi_in midi = *pmidi;
-    midi.prev_keys[pitch] = midi.keys[pitch];
-    midi.keys[pitch] = velocity;
+#ifdef PRINT
+    Serial.println("off");
+#endif
+    pmidi->prev_keys[pitch] = pmidi->keys[pitch];
+    pmidi->keys[pitch] = 0;
 }
 
 void midi_in_get_cmd(midi_in* pmidi) {
-    midi_in midi = *pmidi;
     int input = midi_in_get_raw();
 
     if (input != 0) {
+
         // forces it to have data
-        switch (midi.counter) {
+        switch (pmidi->counter) {
             case 0:
                 // status byte
-                midi.cmd[0] = input;
-                midi.counter++;
+                pmidi->cmd[0] = input;
+                pmidi->counter++;
                 break;
             case 1:
-                midi.cmd[1] = input;
-                midi.counter++;
+                pmidi->cmd[1] = input;
+                pmidi->counter++;
                 break;
             case 2:
-                midi.cmd[2] = input;
-                midi.counter = 0;
+                pmidi->cmd[2] = input;
+                pmidi->counter = 0;
                 break;
         }
 
@@ -96,35 +95,43 @@ void midi_in_get_cmd(midi_in* pmidi) {
 }
 
 void midi_in_parse_cmd(midi_in* pmidi) {
-    midi_in midi = *pmidi;
+    //midi_in midi = *pmidi;
 
-    if (midi.cmd[2] != 0) {
+
+    if (pmidi->cmd[2] != 0) {
+    //if (1) {
+
+        //Serial.println("READING");
+        //Serial.println("PARSING");
+
         // interpret the status byte
-        byte status = midi.cmd[0] & 240;
-        byte channel = midi.cmd[0] & 15;
+        byte status = pmidi->cmd[0] & 240;
+        byte channel = pmidi->cmd[0] & 15;
 
         // only one available channel
-        if (channel == CHANNEL) {
+        //if (channel == CHANNEL) {
+        if (1) {
 
             // TODO: expand the number of cmds
             switch (status) {
                 case 128:
-                    midi_in_note_off(pmidi, midi.cmd[1], midi.cmd[2]);
-                    Serial.println("off");
-                    Serial.println(midi.cmd[1]);
-                    Serial.println(midi.cmd[2]);
+                    midi_in_note_off(pmidi, pmidi->cmd[1], pmidi->cmd[2]);
+                    //Serial.println("off");
+                    //Serial.println(midi.cmd[1]);
+                    //Serial.println(midi.cmd[2]);
                     break;
                 case 144:
-                    midi_in_note_off(pmidi, midi.cmd[1], midi.cmd[2]);
-                    Serial.println("on");
-                    Serial.println(midi.cmd[1]);
-                    Serial.println(midi.cmd[2]);
+                    midi_in_note_on(pmidi, pmidi->cmd[1], pmidi->cmd[2]);
+                    //Serial.println("on");
+                    //Serial.println(pmidi->cmd[1]);
+                    //Serial.println(pmidi->cmd[2]);
                     break;
             }
-            //midi.cmd = {0, 0, 0};
-            midi.cmd[0] = 0;
-            midi.cmd[1] = 0;
-            midi.cmd[2] = 0;
+
+            // resets the message
+            pmidi->cmd[0] = 0;
+            pmidi->cmd[1] = 0;
+            pmidi->cmd[2] = 0;
 
         }
     }
--- picosyn.ino
+++ picosyn.ino
@@ -2,14 +2,73 @@
 #include "midi.h"
 
 void setup() {
+    pinMode(25, OUTPUT);
+
+    // only for print debuging :D
     Serial.begin(9600);
+
+    Serial1.setRX(1);
     Serial1.begin(31250);
 
+    // wait untill ready, otherwise no printing or input
+    while (!Serial) {
+    }
+    while (!Serial1) {
+    }
+
+
+    midi_in *midi = midi_in_init();
+    if (midi != NULL) {
+        Serial.println("initialized");
+    }
+
+    int toggle = 0;
+
     while (1) {
-        Serial.print("hi");
-        delay(1000);
-        Serial.print("bye");
-        delay(1000);
+
+        //if (midi->counter > 0) {
+            //Serial.println(midi->counter);
+        //}
+        //Serial.println("INFINITY");
+        midi_in_get_cmd(midi);
+        midi_in_parse_cmd(midi);
+
+        //if (midi->keys[60] != 0) {
+        //    Serial.println(midi->keys[60]);
+        //}
+
+
+        // this doesn't work, cause prev keys and keys don't get reset the right way.
+
+        if (midi->counter == 0) {
+            for (int i = 0; i < 127; i++) {
+                if (midi->keys[i] != 0 && midi->prev_keys[i] == 0) { // it's on, but for how long? 
+                    //Serial.println("NOTE ON");
+                    Serial.print("Note on at: midi ");
+                    Serial.print(i);
+                    Serial.print(" velocity ");
+                    Serial.println(midi->keys[i]);
+                    midi->prev_keys[i] = midi->keys[i];
+                    toggle = 1;
+                } else if (midi->keys[i] == 0 && midi->prev_keys[i] != 0) {
+                    Serial.print("Note off at: midi ");
+                    Serial.println(i);
+                    midi->prev_keys[i] = midi->keys[i];
+                    toggle = 0;
+                }
+            }
+        }
+
+        if (toggle == 1) {
+            digitalWrite(25, HIGH);
+        } else if (toggle == 0) {
+            digitalWrite(25, LOW);
+        }
+
+        //Serial.print("hi");
+        //delay(1000);
+        //Serial.print("bye");
+        //delay(1000);
 
     }
 }
